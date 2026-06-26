25bceb0f4f1d26d06c27d94d5906d123df369852
Ranjeet Kumar <rk5992780@gmail.com>
2026-06-27 01:50:21 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,23 +1,11 @@
-const mysql = require("mysql2");
-
-console.log("HOST:", process.env.DB_HOST);
-console.log("USER:", process.env.DB_USER);
-console.log("DB:", process.env.DB_NAME);
-
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
-
 db.connect((err) => {
   if (err) {
-    console.log("FULL DB ERROR:", err);
-    console.log("🔥 DB FILE LOADED");
+    console.log("CODE:", err.code);
+    console.log("ERRNO:", err.errno);
+    console.log("SQLSTATE:", err.sqlState);
+    console.log("MESSAGE:", err.sqlMessage);
+    console.log(err);
   } else {
     console.log("✅ MYSQL CONNECTED");
   }
 });
-
-module.exports = db;
