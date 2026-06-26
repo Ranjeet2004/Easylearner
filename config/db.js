8a5e6bb8f7226d24c0bfa86fa00abd8e9fa5156b
Ranjeet Kumar <rk5992780@gmail.com>
2026-06-27 01:54:57 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,11 +1,28 @@
+require("dotenv").config();
+
+const mysql = require("mysql2");
+
+console.log("HOST:", process.env.DB_HOST);
+console.log("USER:", process.env.DB_USER);
+console.log("DB:", process.env.DB_NAME);
+
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
 db.connect((err) => {
   if (err) {
     console.log("CODE:", err.code);
     console.log("ERRNO:", err.errno);
     console.log("SQLSTATE:", err.sqlState);
     console.log("MESSAGE:", err.sqlMessage);
-    console.log(err);
+    console.log("FULL DB ERROR:", err);
   } else {
     console.log("✅ MYSQL CONNECTED");
   }
 });
+
+module.exports = db;
