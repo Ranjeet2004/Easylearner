559298fd7f7fa3a7ee09433bcb3e2312a4273936
Ranjeet Kumar <rk5992780@gmail.com>
2026-06-27 17:17:00 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,17 +3,17 @@ require("dotenv").config();
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: "srv1946.hstgr.io",
+  user: "u203717998_Easyproject",
+  password: "Easy@123456789",
+  database: "u203717998_Easyproject",
 });
 
 db.connect((err) => {
   if (err) {
     console.log(err);
   } else {
-    console.log("✅ MYSQL CONNECTED");
+    console.log("CONNECTED");
   }
 });
 
