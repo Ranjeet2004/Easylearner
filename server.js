f48bd365a5f879edffde5b2d9060a87b2c12e1e3
Ranjeet Kumar <rk5992780@gmail.com>
2026-06-27 16:38:46 +0530

    Reorganize imports in server.js
    Moved express and cors imports to the top of the file.

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ require("dotenv").config();
 
 require("./config/db");
 
+const express = require("express");
+const cors = require("cors");
+
 console.log("DB_HOST =", process.env.DB_HOST);
 console.log("DB_USER =", process.env.DB_USER);
 console.log("DB_NAME =", process.env.DB_NAME);
 console.log("DB_PASSWORD =", process.env.DB_PASSWORD ? "SET" : "NOT SET");
-
-const express = require("express");
-const cors = require("cors");
 
 const app = express();
 
