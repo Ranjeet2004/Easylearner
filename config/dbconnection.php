1561c82b200bc0678235587ad896bad9460a3b86
Ranjeet Kumar <rk5992780@gmail.com>
2026-06-27 18:23:47 +0530

    Update dbconnection.php

--- config/dbconnection.php
+++ config/dbconnection.php
@@ -1,1 +1,21 @@
+<?php
 
+$host = "srv1946.hstgr.io";
+$dbname = "u203717998_Easyproject";
+$username = "u203717998_Easyproject";
+$password = "Easy@123456789";
+
+try {
+    $conn = new PDO(
+        "mysql:host=$host;dbname=$dbname;charset=utf8",
+        $username,
+        $password
+    );
+
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+} catch (PDOException $e) {
+    die("Connection Failed: " . $e->getMessage());
+}
+
+?>
